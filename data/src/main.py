948d934d1228a14f3e6a527fafc2f0289ad55b45
pseudoaj <ajaykrishnateja@gmail.com>
2017-01-22 12:25:07 -0500

    Added the user names and business names

--- data/src/main.py
+++ data/src/main.py
@@ -17,20 +17,27 @@ from faker import Faker
 class DataEngine():
 
     # Initialization
-    def __init__(self,numOfUsrs,intrvl):
+    def __init__(self,numOfRegs,intrvl):
 
-        # Assign the number of users to create
-        self.numOfUsrs = numOfUsrs
+        # Assign the number of registrations to create
+        self.numOfRegs = numOfRegs
 
         # Assign the interval for the sensors
         self.intrvl = intrvl
+
+        # Assign a percentile of business
+        self.bizPrcnt = 6
+
+        # Assign the percentile of users and business
+        self.numOfBizs = int(self.bizPrcnt*self.numOfRegs/100)
+        self.numOfUsrs = self.numOfRegs-self.numOfBizs
 
         # Initialize the faker class for generating data
         self.faker = Faker()
 
         # Debug statement
         print "#====================#"
-        print "Method initialized with "+str(self.numOfUsrs)+" users and interval of "+str(self.intrvl)
+        print "Method initialized with "+str(self.numOfUsrs)+" users, "+str(self.numOfBizs)+" business and interval of "+str(self.intrvl)
 
     # Method to trigger the data
     # Creates the users and then uses the users to generate the utility info
@@ -48,8 +55,8 @@ class DataEngine():
         usrNme = self.faker.name()
 
         # debug statement
-        print "#====================#"
-        print "Current user details: "+str(usrNme)
+        # print "#====================#"
+        # print "Current user details: "+str(usrNme)
 
     # Method to generate company profile
     def genCmpny(self):
@@ -58,17 +65,17 @@ class DataEngine():
         cmpnyNme = self.faker.company()
 
         # debug statement
-        print "#====================#"
-        print "Current company details: "+str(cmpnyNme)
+        # print "#====================#"
+        # print "Current company details: "+str(cmpnyNme)
 
 if __name__ == '__main__':
 
     # Define the variables
-    numOfUsrs = 50
+    numOfRegs = 50
     intrvl = 5
 
     # Initialize the main class and run through
-    thisEngne =  DataEngine(numOfUsrs,intrvl)
+    thisEngne =  DataEngine(numOfRegs,intrvl)
 
     # Triger the data generation
     thisEngne.generate()
