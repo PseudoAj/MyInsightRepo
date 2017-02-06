94706d5657242c67e988a9c80bac6196f5b172a3
pseudoaj <ajaykrishnateja@gmail.com>
2017-02-06 17:26:31 -0500

    Corrected init and conf files

--- setup/laravel_init.sh
+++ setup/laravel_init.sh
@@ -8,7 +8,7 @@
 #==============================================================================
 
 # variables
-PROJECT_NAME="Auth"
+PROJECT_NAME="auth"
 WEB_ROOT="/var/www/html/"
 PROJECT_DIR="MyInsightRepo/laravel/"
 
@@ -21,7 +21,15 @@ cd $PROJECT_DIR
 
 # Create a sample laravel project
 # laravel new project-css
-composer create-project --prefer-dist laravel/laravel PROJECT_NAME
+composer create-project --prefer-dist laravel/laravel $PROJECT_NAME
 
 # Move into Apache
 sudo mv $PROJECT_DIR$PROJECT_NAME $WEB_ROOT
+
+# Set the configurations
+chmod 775 $WEB_ROOT$PROJECT_NAME
+sudo chown -R apache:apache $WEB_ROOT$PROJECT_NAME
+sudo chmod 755 /var/www
+# sudo rm -R /etc/httpd/conf/httpd.conf
+# sudo ln -s /vagrant/serverConfiguration/httpd.conf /etc/httpd/conf
+sudo service apache2 restart
