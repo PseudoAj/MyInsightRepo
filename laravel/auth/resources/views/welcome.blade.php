8f06062bc27206d002d11b60140ac4dfdd43a62d
pseudoaj <ajaykrishnateja@gmail.com>
2017-02-06 17:57:39 -0500

    Modified test view

--- laravel/auth/resources/views/welcome.blade.php
+++ laravel/auth/resources/views/welcome.blade.php
@@ -81,14 +81,12 @@
                 <div class="title m-b-md">
                     Auth
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <p>
+                  Analytics on Utility Things
+                </p>
+                <p>
+                  By Ajay Kavuri
+                </p>
             </div>
         </div>
     </body>
