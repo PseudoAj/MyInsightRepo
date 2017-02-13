972d68456842aa53633b2db450bf9c51bcb43f9f
pseudoaj <ajaykrishnateja@gmail.com>
2017-02-12 19:28:10 -0500

    Better logic for stream generation

--- data/stream/electricity.py
+++ data/stream/electricity.py
@@ -13,6 +13,7 @@ import datetime
 import random
 import traceback
 import csv
+import time
 import sys
 import pickle
 from kafka import KafkaProducer
@@ -26,6 +27,19 @@ class Electricity():
 
     # Initialize
     def __init__(self,duration=1,intrvl=5,cnctnAddr='default'):
+
+        # Define address
+        self.cnctnAddr = cnctnAddr
+
+        # Define the intrvl
+        self.intrvl = datetime.timedelta(seconds=int(intrvl))
+
+        # Define step size
+        self.step = datetime.timedelta(seconds=1)
+
+        # Define the duration
+        self.duration = datetime.timedelta(days=int(duration))
+
         # Define the directory to output
         self.regDataDir = "../output/registrations/"
 
@@ -51,6 +65,15 @@ class Electricity():
 
         # Get the number of users
         self.numOfRegs = self.metaDataDict.get('numOfRegs')
+
+        # Get the end time
+        self.endTime = self.strtTime+self.duration
+
+        # Initialize the class
+        self.producer = KafkaProducer(bootstrap_servers=self.cnctnAddr)
+
+        # define the topic you want to send
+        self.topic = 'electricity'
 
         # Debug statement
         # print "Starting time: "+str(self.strtTime)+" Number of registrations: "+str(self.numOfRegs)
@@ -98,6 +121,124 @@ class Electricity():
         except Exception:
             traceback.print_exc()
 
+    # Method to start the producer
+    def produce(self):
+
+        # Get the current time
+        curTime = self.strtTime
+
+        # Run through for the time
+        while curTime < self.endTime:
+
+            # Get the loop start time
+            loopStrtTime = datetime.datetime.now()
+
+
+            # Convert the current time into a unix time stamp
+            curStrTime = curTime.strftime("%s")
+
+            # New time stamp
+            newTime = curTime + self.intrvl
+            newTimeStmp = newTime.strftime("%s")
+
+            # check if it exists in the time dict
+            if int(curStrTime) in self.timeDataDict:
+
+                # write a value for this data
+                # get the service_id
+                curSrvceList = self.timeDataDict.get(int(curStrTime))
+
+                # for all the service id write the record
+                for curSrvceId in curSrvceList:
+                    # Get the dictionary
+                    curRcrdDict = self.regDataDict.get(str(curSrvceId))
+
+                    # pass on the dict to wite the csv file
+                    curRcrdRow = self.genElecRow(curRcrdDict,int(curStrTime),int(newTimeStmp))
+
+                    # send the data in producers
+                    # Append them as a csv row
+                    curRcrdRowStr = self.convrtLstToCSV(curRcrdRow)
+
+                    # send them through the producer
+                    self.produceStream(curSrvceId, curRcrdRowStr)
+
+                    # # Debug statement
+                    print curRcrdRowStr
+
+                    # # Debug statement
+                    # print "Writing: "+str(curSrvceId)+" at time: "+str(curStrTime)
+                    # raw_input()
+
+                # update the next time stamp by pop and insert
+                # print self.timeDataDict.get(int(curStrTime))
+                # Check if the timestamp already exists
+                if int(newTimeStmp) in self.timeDataDict:
+                    # Append to existing list
+                    self.timeDataDict[int(newTimeStmp)] = self.timeDataDict.get(int(newTimeStmp))+self.timeDataDict.get(int(curStrTime))
+                else:
+                    # Else just create a new list
+                    self.timeDataDict[int(newTimeStmp)] = self.timeDataDict.get(int(curStrTime))
+                # remove the element
+                self.timeDataDict.pop(int(curStrTime))
+
+            # increment curTime
+            curTime+=self.step
+
+            # Get the time for loop end
+            loopEndTime = datetime.datetime.now() - loopStrtTime
+            loopEndTime = loopEndTime.total_seconds()
+            remngTime = float(1-loopEndTime)
+
+            # Sleep for that amount of time
+            time.sleep(remngTime)
+
+            # Debug statement
+            print remngTime
+            # print "Current time: "+str(curStrTime)+" ,next timestamp: "+str(newTimeStmp)+" dict: "+str(self.timeDataDict)
+            # raw_input()
+
+    # Function for reading the dictionary and generating a consumption rate
+    def genElecRow(self,rcrdDict,startTime,endTime):
+
+        # variable to store the row
+        curRcrd = []
+
+        # get all the attributes
+        service_id = rcrdDict.get('service_id')
+        user_name = rcrdDict.get('user_name')
+        type = rcrdDict.get('type')
+        strtTime = rcrdDict.get('strtTime')
+        state = rcrdDict.get('state')
+        consumption = float(random.triangular(0,0.30))
+
+        # append the attributes to one list
+        curRcrd.append(service_id)
+        curRcrd.append(user_name)
+        curRcrd.append(type)
+        curRcrd.append(startTime)
+        curRcrd.append(state)
+        curRcrd.append(consumption)
+        curRcrd.append(endTime)
+
+        # return the list
+        return curRcrd
+
+    # Method to write the data into kafka
+    def produceStream(self,curKey,curData):
+
+        # send the data
+        self.producer.send(self.topic, key=str(curKey), value=curData)
+
+    # function to convert given list of strings and integers to a csv line
+    def convrtLstToCSV(self, lst):
+
+        # covert each part as string
+        for idx,rcrd in enumerate(lst):
+            lst[idx] = str(rcrd)
+
+        # return with join
+        return ",".join(lst)
 
 # Main method
 if __name__ == '__main__':
@@ -107,5 +248,17 @@ if __name__ == '__main__':
     # Debug statement
     print 'Argument List:', str(sys.argv)
 
+    # Get the arguments
+    ## duration
+    dur = int(sys.argv[1])
+    ## Interval
+    intrvl = int(sys.argv[2])
+    ## Producer address
+    addr = sys.argv[3]
+
+
     # Initialize the class
-    thisElcProdcr =  Electricity()
+    thisElcProdcr =  Electricity(dur,intrvl,addr)
+
+    # Start the producer method
+    thisElcProdcr.produce()
