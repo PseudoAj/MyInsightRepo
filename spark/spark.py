8078b8e43b618ff7dece06f3534eb13b4eab02d7
pseudoaj <ajaykrishnateja@gmail.com>
2017-02-17 15:52:58 -0500

    Creating spark code

---
+++ spark/spark.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#title           :spark.py
+#description     :The current python file reads the data from s3 and runs job on top of it
+#author		     :Ajay Krishna Teja Kavuri
+#date            :02012017
+#version         :0.1
+#==============================================================================
+
+# Libraries
+
+#==============================================================================
+
+# Implementation
+
+class Spark():
+
+    # Initialize
+    def __init__(self):
+
+        # Debug statement
+        print "Initialized"
+
+# main method
+if __name__ == '__main__':
+
+    # Get the arguments that you need
+
+    # Initialize the class
+    thisSparkJob = Spark()
